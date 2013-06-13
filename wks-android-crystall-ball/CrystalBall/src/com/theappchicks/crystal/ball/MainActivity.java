bc78895465a4b5256b8483fb93371f5a318515ef
Carey McLean <carey@theAppChicks.com>
2013-06-13 13:49:31 -0700

    Adding view variables and assigning them views from the layout file.

--- wks-android-crystall-ball/CrystalBall/src/com/theappchicks/crystal/ball/MainActivity.java
+++ wks-android-crystall-ball/CrystalBall/src/com/theappchicks/crystal/ball/MainActivity.java
@@ -1,8 +1,10 @@
 package com.theappchicks.crystal.ball;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Declare our View Variables and assign them views from the layout file
+        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        Button answerButton = (Button) findViewById(R.id.button1);
     }
 
 
